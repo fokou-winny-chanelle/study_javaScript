74a5c1e0b145f7ebf66f06a04b367c4bb1dea569
fokou <fokouchanelle@gmail.com>
2023-05-12 08:52:42 +0100

    study js

---
+++ java_script/1.js
@@ -0,0 +1,24 @@
+// string sata type
+// some methods of the string datatype
+
+let email = "chanelle@gmail.com";
+console.log(email.slice(2,12));
+
+// not a number Nan
+console.log(6/"ad2");
+let val = 16;
+
+// backsticks
+conqa = `<br>i am winny and my email is ${email} and i am ${val} years old `;
+console.log(conqa);
+document.write(conqa);
+
+// OBJECTS datatype
+// 1)arrays
+
+let arr = [1,2,3,4,5,6];
+console.log("array = ",arr);
+console.log("array[4] = ",arr[4]);
+// overwrite an array
+arr[4] = 9;
+console.log("modified array = ",arr);
---
+++ java_script/first.js
@@ -0,0 +1,6 @@
+const x1 = 2;
+const x2 = 5; 
+var x3 = x1 + x2 ;
+console.log("x1 + x2 = "+ x3)
+window.alert("x1 + x2 = "+ x3)
+document.write("x1 + x2 = "+ x3)
